ebdf85b6ddd50b31155d7ff713d40748471952a5
njeff <jefftiger99@gmail.com>
2013-09-28 10:47:34 -0700

    Added comments at the top.

--- Prototype/src/prototype/DatabaseClass.java
+++ Prototype/src/prototype/DatabaseClass.java
@@ -1,6 +1,5 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * Code for starting the database connection and svaing songs to the database
  */
 package prototype;
 
