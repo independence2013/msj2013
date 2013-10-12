0e708d5cf08e62ebe30c212b7f626387619a69df
njeff <jefftiger99@gmail.com>
2013-10-12 12:03:24 -0700

    Added keywords object.

---
+++ Prototype/src/prototype/Keywords.java
@@ -0,0 +1,15 @@
+/*
+ * Keyword object
+ */
+package prototype;
+/**
+ * @author Jeffrey
+ */
+
+//object for keyword array
+public class Keywords {
+    public String keyword;
+    public int valence;
+    public int arousal;
+    public int dominance;
+}
