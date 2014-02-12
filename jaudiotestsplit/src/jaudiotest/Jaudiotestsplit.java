6392f205b69ef6254cb25e91c65a9a79815ea507
njeff <jefftiger99@gmail.com>
2014-02-11 18:21:39 -0800

    Now works with cut song data file structure (song is cut up and put into its own folder).

--- jaudiotestsplit/src/jaudiotest/Jaudiotestsplit.java
+++ jaudiotestsplit/src/jaudiotest/Jaudiotestsplit.java
@@ -20,20 +20,11 @@ import java.util.logging.Logger;
 public class Jaudiotestsplit {
     
     public static void main(String[] args) throws Exception {
-        File[][] allfiles = new File[8][];
+        String[][] alldirectories = new String[8][];
         for(int x = 0; x<8; x++){
-            String mdir = "F:\\Jeffrey\\Music\\Songs\\wav\\" + x; //directory for songs
-            File musicdir = new File(mdir);
-            allfiles[x]= musicdir.listFiles(new FilenameFilter(){
-                @Override
-                public boolean accept(File dir, String name){
-                    if(name.toLowerCase().endsWith(".wav")){
-                        return true;
-                    }
-                    return false;
-                }
-            }
-            ); 
+            String fdir = "F:\\Jeffrey\\Desktop\\individual\\cut songs\\" + x; //directory for songs
+            File file = new File(fdir);
+            alldirectories[x] = file.list();
         }
         int windowSize = 512; //size of the analysis window in samples
         double windowOverlap = 0; //percent overlap as a value between 0 and 1
@@ -58,21 +49,33 @@ public class Jaudiotestsplit {
         DataModel dm = new DataModel("features.xml",null);
         //File[] tempfile = new File[1];
         for(int j = 0; j<8; j++){
-            //for(int z = 0;z<allfiles[j].length; z++){
+            for(int z = 0; z<alldirectories[j].length; z++){
+                String mdir = "F:\\Jeffrey\\Desktop\\individual\\cut songs\\" + j + "\\" + alldirectories[j][z]; //directory for songs
+                File musicdir = new File(mdir);
+                File[] allfiles = musicdir.listFiles(new FilenameFilter(){ //use filter to make sure we don't read any album art files (.jpg)
+                @Override
+                public boolean accept(File dir, String name){
+                    if(name.toLowerCase().endsWith(".wav")){
+                        return true;
+                    }
+                    return false;
+                    }
+                }
+                );
                 for (int i = 0; i < o.length; ++i) {
                     //System.out.println(allfiles[j][z].getName());
                     //tempfile[0] = allfiles[j][z];
-                    featureDestination = "F:\\Jeffrey\\Desktop\\individual\\44_1_MFCC_nodev\\Whole Song Data (Classifier)\\"+j+".arff"; //location where extracted features should be stored
+                    featureDestination = "F:\\Jeffrey\\Desktop\\individual\\cut songs\\ARFFData\\"+j+"\\"+ alldirectories[j][z] +".arff"; //location where extracted features should be stored
                     b = (Batch) o[i];
                     b.setDestination(featureLocation,featureDestination);
                     dm.featureKey = new FileOutputStream(new File(b.getDestinationFK()));
                     dm.featureValue = new FileOutputStream(new File(b.getDestinationFV()));
                     b.setDataModel(dm);
                     b.setSettings(windowSize,windowOverlap,samplingRate,normalize,perWindow,overall,outputType);
-                    b.setRecordings(allfiles[j]);
+                    b.setRecordings(allfiles);
                     b.execute();
                 }
-            //}
+            }
         }
     }
 }
