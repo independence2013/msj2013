716e236e189eded92771eb422dba18f69c007c20
njeff <jefftiger99@gmail.com>
2014-02-11 17:26:48 -0800

    Code to split an audio file into 15 second chunks.

---
+++ audiosplitter/src/audiosplitter/Audiosplitter.java
@@ -0,0 +1,91 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package audiosplitter;
+
+import java.io.File;
+import java.io.FileInputStream;
+import javax.sound.sampled.AudioFileFormat;
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.Line;
+
+
+/**
+ *
+ * @author Jeffrey
+ */
+public class Audiosplitter {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws Exception {
+        File[] files = new File[1];
+        File filenew = new File("F:\\Jeffrey\\Music\\Songs\\wav\\0\\Dynamite.wav");
+        files[0] = filenew;
+        split(files);
+    }
+    
+    public static void split(File[] files) throws Exception{
+        for(int i = 0; i < files.length; i++){
+            AudioInputStream stream = null;
+            stream = AudioSystem.getAudioInputStream(files[i]);
+            AudioFormat format = stream.getFormat();
+            long filelength = files[i].length(); //length in bytes
+            int bytes_p_sec = (int)format.getSampleRate()*format.getSampleSizeInBits()*format.getChannels()/8;
+            long fileduration = filelength/bytes_p_sec;
+            AudioInputStream shortstream = null;
+            int copylength = (int)format.getSampleRate()*15; //15 seconds
+            //System.out.println(copylength);
+            int timestocut = (int)Math.ceil(fileduration/15);
+            //File tempFile = null;
+            for(int j = 0; j <= timestocut; j++){
+                //System.out.println(stream.available());
+                if(copylength*format.getSampleSizeInBits()*format.getChannels()/8 > stream.available()){ //if there isn't 15 seconds of audio left
+                    shortstream = new AudioInputStream(stream,format,stream.available()*8/(format.getSampleSizeInBits()*format.getChannels())); //make the stream only until the end
+                }
+                else {
+                    shortstream = new AudioInputStream(stream,format,copylength);
+                }
+                File tempFile = new File("F:\\Jeffrey\\Desktop\\individual\\cut songs\\" + files[i].getName() + j + ".wav");
+                tempFile.createNewFile();
+                AudioSystem.write(shortstream, AudioFileFormat.Type.WAVE, tempFile);
+                
+                
+                
+                //tempFile.delete();
+            }
+            
+            stream.close();
+            shortstream.close();
+        }
+    }
+    
+    /*
+    public static byte[] fileToByteArray(File tempfile) throws Exception{
+        byte[] output = new byte[2];
+        FileInputStream fi = new FileInputStream(tempfile);
+        int data;
+        short datatwo;
+        boolean secondbyteflag = false;
+        while((data = fi.read()) != -1){
+            if(!secondbyteflag){
+                output[0] = (byte)data;
+                secondbyteflag = true;
+            }
+            else{
+                output[1] = (byte)data;
+                secondbyteflag = false;
+                datatwo = (short) ((output[0] << 8) | (output[1] & 0xFF));
+                System.out.println(datatwo);
+            }
+        }
+        fi.close();
+        return output;
+    }
+    */
+}
