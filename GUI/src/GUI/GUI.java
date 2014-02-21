673c551a3504ded01c5d8a9c1db2a500159fde19
njeff <jefftiger99@gmail.com>
2014-02-20 16:10:06 -0800

    Relative image path.

--- GUI/src/GUI/GUI.java
+++ GUI/src/GUI/GUI.java
@@ -632,7 +632,7 @@ public class GUI extends javax.swing.JFrame {
                 .addComponent(jLabel25)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(jLabel27, javax.swing.GroupLayout.DEFAULT_SIZE, 126, Short.MAX_VALUE)
+                    .addComponent(jLabel27, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addComponent(jLabel11, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(audioProgressSlider1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -1083,7 +1083,7 @@ public class GUI extends javax.swing.JFrame {
         x = true;
         clip.stop();
         clip.setMicrosecondPosition(0);
-        //thread.interrupt();
+        
         audioProgressSlider1.setValue(0);
         audioProgress1.setValue(0);
     }//GEN-LAST:event_stopButton1ActionPerformed
